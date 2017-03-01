97f0eda6e69d6393ea11d584a4279c3d00e22a36
Julia Garcia <jg439@njit.edu>
2017-03-01 13:40:51 -0500

    first commit

---
+++ app/src/main/java/app/firstapiaiapp/example/com/firstaiapiapp/MainActivity.java
@@ -0,0 +1,82 @@
+package app.firstapiaiapp.example.com.firstaiapiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//Importing text buttons and views from activity_main.xml
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+//Importing ai.api classes
+import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+import com.google.gson.JsonElement;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements AIListener{
+
+
+    private Button listenButton;
+    private TextView resultTextView;
+
+    private AIService aiService;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listenButton = (Button) findViewById(R.id.listenButton);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+        final AIConfiguration config = new AIConfiguration("209dcc92a8f5400da3f6d76b8f90d758",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+
+
+    }
+
+    public void listenButtonOnClick(final View view) {
+        aiService.startListening();
+    }
+
+    public void onResult(final AIResponse response) {
+        Result result = response.getResult();
+
+        // Get parameters
+        String parameterString = "";
+        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
+            }
+        }
+
+        // Show results in TextView.
+        resultTextView.setText("Query:" + result.getResolvedQuery() +
+                "\nAction: " + result.getAction() +
+                "\nParameters: " + parameterString);
+    }
+
+    @Override
+    public void onError(final AIError error) {
+        resultTextView.setText(error.toString());
+    }
+    @Override
+    public void onListeningStarted() {}
+
+    @Override
+    public void onListeningCanceled() {}
+
+    @Override
+    public void onListeningFinished() {}
+
+    @Override
+    public void onAudioLevel(final float level) {}
+
+}
